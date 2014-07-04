ea7fd04f4528666814b1323ffd50ac623cfeab15
dancingshell <kaitlinjaffe@gmail.com>
2014-07-04 14:28:35 -0700

    changes to code readability, user readability, and fixed bug that didn't ask if new user after 2nd game

--- mad_libs_game3.rb
+++ mad_libs_game3.rb
@@ -12,47 +12,49 @@ def madlibs
   quote = "I've learned that noun will verb what you said, noun will verb what you did, but noun will never verb how you made them verb"
   passage = quote.split
 
-  puts "Mad Libs!"
-  question = "Would you like to start a new game? Y/N"
+  
+  puts "\nMAD LIBS!!!"
+
+  question = "\nWould you like to start a new game? Y/N"
   puts question
   answer = gets.chomp
+  puts ""
   answer.downcase!
-  counter = 1
-  
 
+  counter = 0
+  
   if answer == "y"
     fetch_name()
 
-    if counter == $game_counter ||= 0
-      puts "new_user? Y/N"
+    if counter < $game_counter ||= 0
+      puts "new_user? Y/N\n"
       @new_user_response = gets.chomp
       @new_user_response.downcase!
       
       if @new_user_response == "y"
+        puts ""
         fetch_name()
       end
     end
 
     for i in 0...passage.length
       if passage[i].include?("noun")
-        puts "Give me a noun, #{$user_name}."
+        puts "\nGive me a noun, #{$user_name}."
         passage[i] = gets.chomp
-        puts ""
+
       elsif passage[i].include?("verb")
-        puts "Give me a verb, #{$user_name}."
+        puts "\nGive me a verb, #{$user_name}."
         passage[i] = gets.chomp
-        puts ""
       end
 
     end
     $game_counter += 1
     puts ""
     puts passage.join(" ") + ".\n-#{$user_name}"
-    puts ""
     madlibs #recursive method (plays game again)
 
   elsif answer == "n"
-    puts "Goodbye!"
+    puts "\nGoodbye!"
   else 
     puts "Please choose Y/N." 
   end    
